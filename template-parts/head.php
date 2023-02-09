7e853516d99a093e13e58cea756307999ffb079e
graafmans.tom@gmail.com <xarxino>
2023-02-09 10:50:38 +0100

    Moved head section to seperate file

---
+++ template-parts/head.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<?php $meta_desc = "Hey there! I'm a freelance UI designer ready to add some pizzazz to your digital world. I specialize in creating user-friendly and visually appealing designs that not only look good, but solve real problems for your users. Whether you need a website, app, or any other digital product, I've got you covered. Let's chat and see how I can bring your ideas to life and give your users the seamless experience they deserve." ?>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <!-- Primary Meta Tags -->
+    <title><?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?></title>
+    <meta name="title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
+    <meta name="description" content="<?php echo $meta_desc; ?>">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?php bloginfo('url') ?>">
+    <meta property="og:title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
+    <meta property="og:description" content="<?php echo $meta_desc; ?>">
+    <meta property="og:image" itemprop="image" content="<?php echo get_template_directory_uri(); ?>/src/img/meta_image.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="<?php bloginfo('url') ?>">
+    <meta property="twitter:title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
+    <meta property="twitter:description" content="<?php echo $meta_desc; ?>">
+    <meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/src/img/meta_image.png">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/apple-touch-icon.png">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <?php wp_head(); ?>
+</head>
--- template-parts/header.php
+++ template-parts/header.php
@@ -1,42 +1,6 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<?php include('head.php') ?>
 
-<?php $meta_desc = "Hey there! I'm a freelance UI designer ready to add some pizzazz to your digital world. I specialize in creating user-friendly and visually appealing designs that not only look good, but solve real problems for your users. Whether you need a website, app, or any other digital product, I've got you covered. Let's chat and see how I can bring your ideas to life and give your users the seamless experience they deserve." ?>
-
-<head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<!-- Primary Meta Tags -->
-	<title><?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?></title>
-	<meta name="title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
-	<meta name="description" content="<?php echo $meta_desc; ?>">
-
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="<?php bloginfo('url') ?>">
-	<meta property="og:title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
-	<meta property="og:description" content="<?php echo $meta_desc; ?>">
-	<meta property="og:image" itemprop="image" content="<?php echo get_template_directory_uri(); ?>/src/img/meta_image.png">
-
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image">
-	<meta property="twitter:url" content="<?php bloginfo('url') ?>">
-	<meta property="twitter:title" content="<?php echo get_bloginfo('name') . ' - ' . get_bloginfo('description'); ?>">
-	<meta property="twitter:description" content="<?php echo $meta_desc; ?>">
-	<meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/src/img/meta_image.png">
-
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/favicon-16x16.png">
-	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/site.webmanifest">
-	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/src/img/favicon/apple-touch-icon.png">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="theme-color" content="#ffffff">
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class() ?>>
+<body <?php body_class('overflow-x-hidden') ?>>
 	<header id="#top" class="<?php if (is_front_page()) : echo "full-height pb-8 md:pb-12 lg:pb-16 xl:pb-24";
 								endif; ?>">
 		<nav class="flex items-center justify-between">
@@ -68,7 +32,7 @@
 					<a href="<?php echo esc_url(home_url('/contact')); ?>" class="button">
 						<div>Let's collaborate</div>
 						<i class="ph-thumbs-up-light -rotate-12"></i>
-					</a>
+							</a>
 					<a href="#cases" class="flex items-center justify-center gap-4 opacity-50">
 						<div class="font-display uppercase text-xl">Case studies</div>
 						<i class="ph-arrow-bend-right-down"></i>
