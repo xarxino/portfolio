a2049b148e3a97a5827e7d86af146956d273b7d5
graafmans.tom@gmail.com <xarxino>
2023-02-16 19:02:16 +0100

    Removed Phosphor Icons CDN because it was slowing down loading times

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
+
 /**
  * Enqueue scripts and styles.
  */
+
 function portfolio_scripts()
 {
-	wp_enqueue_style('portfolio-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_enqueue_style('portfolio-style', get_stylesheet_directory_uri(), array(), _S_VERSION, 'all');
 	wp_enqueue_script('portfolio-bundle', get_stylesheet_directory_uri() . '/dist/bundle.js', array(), false, true);
-	wp_enqueue_script('phosphor-icons', 'https://unpkg.com/phosphor-icons');
 }
+
 add_action('wp_enqueue_scripts', 'portfolio_scripts');
