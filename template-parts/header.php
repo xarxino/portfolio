b83013d12aa9b82ca44348f7fa4669409ad3358e
graafmans.tom@gmail.com <xarxino>
2023-02-16 19:08:14 +0100

    Separated bg graphics from header file

--- template-parts/header.php
+++ template-parts/header.php
@@ -46,38 +46,6 @@
 					</a>
 				</div>
 			</div>
-			<div class="absolute inset-0 -z-50 opacity-[3%]">
-				<svg width="100%" height="100%" viewBox="0 0 120% 120%" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<circle id="bgCircleOne" fill="url(#gradientOne)" />
-					<defs>
-						<linearGradient id="gradientOne" x1="1000" y1="0" x2="1000" y2="2000" gradientUnits="userSpaceOnUse">
-							<stop offset="0" stop-color="#000000" />
-							<stop offset="1" stop-color="#F1F1F1" stop-opacity="0" />
-						</linearGradient>
-					</defs>
-				</svg>
-			</div>
-			<div class="absolute inset-0 -z-50 opacity-[3%]">
-				<svg width="100%" height="100%" viewBox="0 0 120% 120%" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<circle id="bgCircleTwo" fill="url(#gradientTwo)" />
-					<defs>
-						<linearGradient id="gradientTwo" x1="1000" y1="0" x2="1000" y2="2000" gradientUnits="userSpaceOnUse">
-							<stop offset="0.2" stop-color="#111111" />
-							<stop offset="1" stop-color="#F1F1F1" stop-opacity="0" />
-						</linearGradient>
-					</defs>
-				</svg>
-			</div>
-			<div class="absolute inset-0 -z-50 opacity-[3%]">
-				<svg width="100%" height="100%" viewBox="0 0 120% 120%" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<circle id="bgCircleThree" fill="url(#gradientThree)" />
-					<defs>
-						<linearGradient id="gradientThree" x1="100%" y1="0" x2="100%" y2="2000" gradientUnits="userSpaceOnUse">
-							<stop offset="0.5" stop-color="#222222" />
-							<stop offset="1" stop-color="#F1F1F1" stop-opacity="0" />
-						</linearGradient>
-					</defs>
-				</svg>
-			</div>
+			<?php include('bg-graphics.php') ?>
 		<?php endif; ?>
 	</header>
