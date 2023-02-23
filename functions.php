f934c6c17f24725c8eb2cb7c460ab4cea42515c2
graafmans.tom@gmail.com <xarxino>
2023-02-23 12:46:47 +0100

    Removed unused theme support

--- functions.php
+++ functions.php
@@ -12,7 +12,6 @@ if (!defined('_S_VERSION')) {
 
 function portfolio_setup()
 {
-	load_theme_textdomain('portfolio', get_template_directory() . '/languages');
 	add_theme_support('automatic-feed-links');
 	add_theme_support('title-tag');
 	add_theme_support('post-thumbnails');
