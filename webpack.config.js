f0aaaa642159ea7f9d648b50613f5b7d25ba7686
graafmans.tom@gmail.com <xarxino>
2023-02-23 12:50:29 +0100

    Added source mapping

--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,43 @@ const path = require("path");
 const { EnvironmentPlugin } = require("webpack");
 const isDev = process.env.NODE_ENV === "development";
 const PurgeCSSPlugin = require("purgecss-webpack-plugin");
-const glob = require('glob');
+const glob = require("glob");
 
 module.exports = {
-	mode: isDev ? "development" : "production",
+  mode: isDev ? "development" : "production",
+  devtool: isDev ? "eval" : "source-map",
   entry: "./assets/src/js/index.js",
-	output: {
+  output: {
     path: path.resolve(__dirname, "assets/dist"),
-		filename: "bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					{
-						loader: "postcss-loader",
-						options: {
-							postcssOptions: {
-								plugins: [
-									require("tailwindcss"),
-									require("autoprefixer"),
-								],
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /src\/img\/.*\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: "img",
-						},
-					},
-					{
-						loader: "image-webpack-loader",
-						options: {
-							bypassOnDebug: true,
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new PurgeCSSPlugin({
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new PurgeCSSPlugin({
       paths: glob.sync(`${path.join(__dirname, "assets/src")}/**/*`, {
-				nodir: true,
-			}),
-		}),
-		new EnvironmentPlugin({
-			WP_THEME_URL: process.env.WP_THEME_URL || "http://localhost",
-		}),
-	],
+        nodir: true,
+      }),
+    }),
+    new EnvironmentPlugin({
+      WP_THEME_URL: process.env.WP_THEME_URL || "http://localhost",
+    }),
+  ],
 };
