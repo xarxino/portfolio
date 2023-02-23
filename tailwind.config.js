5adf7ad24e566c1c21d9ad332b795f777b11df77
graafmans.tom@gmail.com <xarxino>
2023-02-23 14:29:22 +0100

    Added fade-out animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.php"],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        'display': ["Lexend", "sans-serif"],
-        'body': ["Mada", "sans-serif"],
-      },
-      colors: {
+	content: ["./**/*.php"],
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ["Lexend", "sans-serif"],
+				body: ["Mada", "sans-serif"],
+			},
+			colors: {
+				primary: "#27332F",
 				dark: {
 					primary: "#CCD8D4", // Primary color
 					secondary: "#121212", // Background color
 				},
-      },
-      backgroundImage: {
+			},
+			backgroundImage: {
+				noise: "url('/assets/src/img/bg-noise.png')",
 				"noise-dark": "url('/assets/src/img/bg-noise-dark.png')",
-    },
+			},
+			animation: {
+				"fade-out": "fadeOut 1s ease",
+			},
+			keyframes: {
+				fadeOut: {
+					"0%": { opacity: 1 },
+					"100%": { opacity: 0 },
+				},
+			},
 			darkMode: "class",
-  },
-  plugins: [],
-}
- 
+		},
+	},
+	plugins: [],
+};
