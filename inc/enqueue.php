aba016bd5eefe6ba024544a3546d7a0c6289180b
graafmans.tom@gmail.com <xarxino>
2023-02-23 14:29:00 +0100

    Changed directory link

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,7 +7,7 @@
 function portfolio_scripts()
 {
 	wp_enqueue_style('portfolio-style', get_stylesheet_directory_uri(), array(), _S_VERSION, 'all');
-	wp_enqueue_script('portfolio-bundle', get_stylesheet_directory_uri() . '/dist/bundle.js', array(), false, true);
+	wp_enqueue_script('portfolio-bundle', get_stylesheet_directory_uri() . '/assets/dist/bundle.js', array(), false, true);
 }
 
 add_action('wp_enqueue_scripts', 'portfolio_scripts');
